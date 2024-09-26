c07bf036493534907854712235a40e0ea578df7e
dhanh2k <dhanh2k@gmail.com>
2024-09-26 16:12:39 +0700

    26/09/2024

--- chess/Piece.js
+++ chess/Piece.js
@@ -1,26 +1,52 @@
 export default class Piece {
-    constructor(x, y, type, color, reverse){
+    constructor(x, y, type, color, reverse) {
         this.pieceElement = document.createElement("div")
         this.pieceElement.classList.add("piece")
         const img = document.createElement("img")
         img.setAttribute("src", `images/pieces/${color}/${type}.png`)
         this.pieceElement.append(img)
- 
+
         this.x = x
         this.pieceElement.style.setProperty("--x", x)
 
         this.y = y
         this.pieceElement.style.setProperty("--y", y)
 
-        if(reverse){
+        this.reverse = reverse
+        if (this.reverse) {
             this.pieceElement.style.setProperty("bottom", "calc(var(--y) * var(--cell-size)")
             this.pieceElement.style.setProperty("right", "calc(var(--x) * var(--cell-size)")
-        }else{
+        } else {
             this.pieceElement.style.setProperty("top", "calc(var(--y) * var(--cell-size)")
             this.pieceElement.style.setProperty("left", "calc(var(--x) * var(--cell-size)")
         }
 
         this.type = type
         this.color = color
+    }
+
+    set setX(value) {
+        this.x = value
+        this.pieceElement.style.setProperty("--x", value)
+    }
+
+    set setY(value) {
+        this.y = value
+        this.pieceElement.style.setProperty("--y", value)
+    }
+
+    rePlacePiece(x, y) {
+        this.setX = x
+        this.setY = y
+        if (this.reverse) {
+            this.pieceElement.style.removeProperty("top")
+            this.pieceElement.style.removeProperty("left")
+            this.pieceElement.style.setProperty("bottom", "calc(var(--y) * var(--cell-size)")
+            this.pieceElement.style.setProperty("right", "calc(var(--x) * var(--cell-size)")
+            
+        } else {
+            this.pieceElement.style.setProperty("top", "calc(var(--y) * var(--cell-size)")
+            this.pieceElement.style.setProperty("left", "calc(var(--x) * var(--cell-size)")
+        }
     }
 }
--- chess/function.js
+++ chess/function.js
@@ -68,10 +68,12 @@ export function setPlacementForMobile(chessboard) {
             chessboard.pieces.forEach(piece => {
                 piece.pieceElement.style.setProperty("--cell-size", "12vw")
             })
+            dragPieceOnMobile(chessboard, chessboard.chessboardElement)
         } else {
             chessboard.pieces.forEach(piece => {
                 piece.pieceElement.style.setProperty("--cell-size", "8vh")
             })
+            dragPieceOnDesktop(chessboard, chessboard.chessboardElement)
         }
     }
 
@@ -83,4 +85,160 @@ export function setPlacementForMobile(chessboard) {
         console.log("Has Changed")
         myFunction(mmObj);
     });
+}
+
+export function dragPieceOnDesktop(chessboard, chessboardElement){
+    let x = undefined, y = undefined
+    chessboard.pieces.forEach(piece => {
+        piece.pieceElement.addEventListener("mousedown", () => {
+            x = piece.x
+            y = piece.y
+            document.addEventListener("mousemove", document.fn = function fn(e) {
+                piece.pieceElement.style.setProperty("cursor", "grabbing")
+                piece.pieceElement.style.setProperty("z-index", "1")
+    
+                if (e.clientY < chessboardElement.offsetTop) {
+                    piece.pieceElement.style.top = -4 + 'vh'
+                    if (chessboard.reverse) {
+                        y = 7
+                    } else {
+                        y = 0
+                    }
+                } else {
+                    if (e.clientY > (chessboardElement.offsetTop + (document.documentElement.clientHeight * 0.64))) {
+                        piece.pieceElement.style.top = 60 + 'vh'
+                        if (chessboard.reverse) {
+                            y = 0
+                        } else {
+                            y = 7
+                        }
+                    } else {
+                        piece.pieceElement.style.top = e.clientY - (chessboardElement.offsetTop) - (document.documentElement.clientHeight * 0.04) + 'px'
+                        if (chessboard.reverse) {
+                            y = 7 - Math.floor((e.clientY - (chessboardElement.offsetTop)) / (document.documentElement.clientHeight * 0.08))
+                        } else {
+                            y = Math.floor((e.clientY - (chessboardElement.offsetTop)) / (document.documentElement.clientHeight * 0.08))
+                        }
+                    }
+                }
+    
+                if (e.clientX < chessboardElement.offsetLeft) {
+                    piece.pieceElement.style.left = -4 + 'vh'
+                    if (chessboard.reverse) {
+                        x = 7
+                    } else {
+                        x = 0
+                    }
+                } else {
+                    if (e.clientX > (chessboardElement.offsetLeft + (document.documentElement.clientHeight * 0.64))) {
+                        piece.pieceElement.style.left = 60 + 'vh'
+                        if (chessboard.reverse) {
+                            y = 0
+                        } else {
+                            y = 7
+                        }
+                    } else {
+                        piece.pieceElement.style.left = e.clientX - (chessboardElement.offsetLeft) - (document.documentElement.clientHeight * 0.04) + 'px'
+                        if (chessboard.reverse) {
+                            x = 7 - Math.floor((e.clientX - (chessboardElement.offsetLeft)) / (document.documentElement.clientHeight * 0.08))
+                        } else {
+                            x = Math.floor((e.clientX - (chessboardElement.offsetLeft)) / (document.documentElement.clientHeight * 0.08))
+                        }
+                    }
+                }
+                console.log("X: ", x, "Y: ", y)
+            })
+    
+            document.addEventListener("mouseup", document.fn1 = function fn1() {
+                piece.pieceElement.style.setProperty("cursor", "grab")
+                piece.pieceElement.style.setProperty("z-index", "0")
+    
+                document.removeEventListener("mousemove", document.fn)
+                document.removeEventListener('mouseup', document.fn1)
+    
+                piece.rePlacePiece(x, y)
+                x = undefined
+                y = undefined
+                console.log(piece)
+            })
+        })
+    })
+}
+
+export function dragPieceOnMobile(chessboard, chessboardElement){
+    let xTouch = undefined, yTouch = undefined
+    chessboard.pieces.forEach(piece => {
+        piece.pieceElement.addEventListener("touchstart", (e) => {
+            e.preventDefault()
+            xTouch = piece.x
+            yTouch = piece.y
+    
+            piece.pieceElement.style.setProperty("transform", "scale(3,3)")
+            piece.pieceElement.style.setProperty("z-index", "1")
+        })
+    
+        piece.pieceElement.addEventListener("touchmove", e => {
+            const touch = [...e.changedTouches][0]
+    
+            if (touch.clientY < chessboardElement.offsetTop) {
+                piece.pieceElement.style.top = -6 + 'vw'
+                if (chessboard.reverse) {
+                    yTouch = 7
+                } else {
+                    yTouch = 0
+                }
+            } else {
+                if (touch.clientY > (chessboardElement.offsetTop + (document.documentElement.clientWidth * 0.96))) {
+                    piece.pieceElement.style.top = 90 + 'vw'
+                    if (chessboard.reverse) {
+                        yTouch = 0
+                    } else {
+                        yTouch = 7
+                    }
+                } else {
+                    piece.pieceElement.style.top = touch.clientY - (chessboardElement.offsetTop) - (document.documentElement.clientWidth * 0.06) + 'px'
+                    if (chessboard.reverse) {
+                        yTouch = 7 - Math.floor((touch.clientY - (chessboardElement.offsetTop)) / (document.documentElement.clientWidth * 0.12))
+                    } else {
+                        yTouch = Math.floor((touch.clientY - (chessboardElement.offsetTop)) / (document.documentElement.clientWidth * 0.12))
+                    }
+                }
+            }
+    
+            if (touch.clientX < chessboardElement.offsetLeft) {
+                piece.pieceElement.style.left = -6 + 'vw'
+                if (chessboard.reverse) {
+                    xTouch = 7
+                } else {
+                    xTouch = 0
+                }
+            } else {
+                if (touch.clientX > (chessboardElement.offsetLeft + (document.documentElement.clientWidth * 0.96))) {
+                    piece.pieceElement.style.left = 90 + 'vw'
+                    if (chessboard.reverse) {
+                        xTouch = 0
+                    } else {
+                        xTouch = 7
+                    }
+                } else {
+                    piece.pieceElement.style.left = touch.clientX - (chessboardElement.offsetLeft) - (document.documentElement.clientWidth * 0.06) + 'px'
+                    if (chessboard.reverse) {
+                        xTouch = 7 - Math.floor((touch.clientX - (chessboardElement.offsetLeft)) / (document.documentElement.clientWidth * 0.12))
+                    } else {
+                        xTouch = Math.floor((touch.clientX - (chessboardElement.offsetLeft)) / (document.documentElement.clientWidth * 0.12))
+                    }
+                }
+            }
+    
+            console.log("X: ", xTouch, "Y: ", yTouch)
+        })
+    
+        piece.pieceElement.addEventListener("touchend", () => {
+            piece.pieceElement.style.setProperty("transform", "scale(1,1)")
+            piece.pieceElement.style.setProperty("z-index", "0")
+            piece.rePlacePiece(xTouch, yTouch)
+            xTouch = undefined
+            yTouch = undefined
+        })
+    })
 }
